4534544654654d30182dbaca39b6f4fecdbac05d
notauserx <notauserx@users.noreply.github.com>
2022-08-28 17:02:28 +0600

    first commit

---
+++ services/product-composite/src/main/java/com/sandcastle/services/productcomposite/ProductCompositeApplication.java
@@ -0,0 +1,13 @@
+package com.sandcastle.services.productcomposite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCompositeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCompositeApplication.class, args);
+	}
+
+}
---
+++ services/product-composite/src/test/java/com/sandcastle/services/productcomposite/ProductCompositeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandcastle.services.productcomposite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCompositeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
