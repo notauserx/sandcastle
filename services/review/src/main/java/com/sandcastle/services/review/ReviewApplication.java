77ed665357b8cb117456bd1fbe02b0f20a333a4d
notauserx <notauserx@users.noreply.github.com>
2022-09-04 11:11:38 +0600

    add config and log connection status on app start

--- services/review/src/main/java/com/sandcastle/services/review/ReviewApplication.java
+++ services/review/src/main/java/com/sandcastle/services/review/ReviewApplication.java
@@ -1,15 +1,23 @@
 package com.sandcastle.services.review;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan("com.sandcastle")
 public class ReviewApplication {
+	private static final Logger LOG = LoggerFactory.getLogger(ReviewApplication.class);
 
 	public static void main(String[] args) {
-		SpringApplication.run(ReviewApplication.class, args);
+
+		ConfigurableApplicationContext ctx = SpringApplication.run(ReviewApplication.class, args);
+
+		String mysqlUri = ctx.getEnvironment().getProperty("spring.datasource.url");
+		LOG.info("Connected to MySQL: " + mysqlUri);
 	}
 
 }
